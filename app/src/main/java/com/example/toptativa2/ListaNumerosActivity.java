5596c3c97a5b7a28681258261e7f9525d576584f
chano <chanotsg@gmail.com>
2019-03-02 08:19:15 -0500

    finalizacion de reservacion de loteria parte final

--- app/src/main/java/com/example/toptativa2/ListaNumerosActivity.java
+++ app/src/main/java/com/example/toptativa2/ListaNumerosActivity.java
@@ -1,12 +1,17 @@
 package com.example.toptativa2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.toptativa2.Adapters.adaNumeros;
+import com.example.toptativa2.Dialogs.InfoDialog;
 import com.example.toptativa2.Models.Juego;
 import com.example.toptativa2.Models.NumericGenerator;
 import com.example.toptativa2.Models.Numero;
@@ -42,16 +47,44 @@ public class ListaNumerosActivity extends AppCompatActivity {
         lv_numeros=(ListView)findViewById(R.id.lv_numeros);
         tv_nom_user=(TextView)findViewById(R.id.tv_nom_user);
         tv_nom_user.setText(usuario.getFullname());
-        if(getNumeros().size()==0){
-            numeros= new ArrayList<>();
-            numeros.add(NumericGenerator.getNumers(8));
-        }
+
+
+        numeros= new ArrayList<>();
+        numeros.add(NumericGenerator.getNumers(8));
         for(int i=0;i<60;i++){
             numeros.add(NumericGenerator.getNumericList(numeros,8));
         }
         adapter= new adaNumeros(ListaNumerosActivity.this,numeros);
         lv_numeros.setAdapter(adapter);
+        lv_numeros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+
+                try {
+                    nds.open();
+                    NumeroJuego nj =nds.verificaJuego( numeros.get(position));
+                    nds.insert(nj);
+                }catch (Exception e){
+
+                }
+
+
+                InfoDialog dialogo = new InfoDialog(ListaNumerosActivity.this,"Respuesta","Ticket reservado", R.drawable.androidtutoria);
+                dialogo.OkButton("Ok",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface alert, int which) {
+                        alert.dismiss();
+                        Intent i = new Intent(ListaNumerosActivity.this,JugarSorteoActivity.class);
+                        startActivity(i);
+                    }
+                });
+                dialogo.make();
+
+
+            }
+        });
 
     }
 
